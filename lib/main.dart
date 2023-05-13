bdffd657a02b3a56cc47c799ae1f323b4283c161
Jawad Rezaei <107846753+jawadRezaei10@users.noreply.github.com>
2023-05-13 11:19:12 +0430

    magic ball completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,58 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: null,
+    MaterialApp(
+      home: BallPage(),
     ),
   );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        title: Text('Ask Me Anything'),
+      ),
+      backgroundColor: Colors.redAccent,
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: TextButton(
+        child: Image.network(_returnUrl(ballNumber)),
+        onPressed: () {
+          setState(() {
+            ballNumber = _randomNum();
+          });
+        },
+      )),
+    );
+  }
+}
+
+int _randomNum() {
+  int num = Random().nextInt(5) + 1;
+  return num;
+}
+
+String _returnUrl(int ballNumber) {
+  //int num = _randomNum();
+  return 'https://raw.githubusercontent.com/londonappbrewery/magic-8-ball-flutter/master/images/ball$ballNumber.png';
 }
